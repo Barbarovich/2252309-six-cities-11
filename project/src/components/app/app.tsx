9c33f967401215ed10d458a0a42476f4482ca130
Barbarovich <anton@barbarovich.ru>
2022-10-31 23:16:05 +0300

    feat(router): Добавит роутинг
    module3-task1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,54 @@
 import Main from '../../pages/main/main';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import {LoginPage} from '../../pages/login-page/login-page';
+import {FavoritesPage} from '../../pages/favorites-page/favorites-page';
+import {RoomPage} from '../../pages/room-page/room-page';
+import {NotFoundPage} from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   availablePlaces: number;
 }
 
 function App({availablePlaces}: AppProps): JSX.Element {
-  return <Main availablePlaces={availablePlaces}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main availablePlaces={availablePlaces}/>}
+        >
+        </Route>
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        >
+        </Route>
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+        >
+        </Route>
+        <Route
+          path={AppRoute.Room}
+          element={<RoomPage/>}
+        >
+        </Route>
+        <Route
+          path="*"
+          element={<NotFoundPage/>}
+        >
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,8 +1,12 @@
 import {FC} from 'react';
+import {AppRoute} from '../../const';
+import {Link} from 'react-router-dom';
+import PageHeader from '../../components/page-header/page-header';
 
 export const NotFoundPage: FC = () => (
-  <>
+  <div className="page">
+    <PageHeader/>
     <h1>Страница не найдена</h1>
-    <link>Перейти на главную</link>
-  </>
+    <Link to={AppRoute.Main}>Перейти на главную</Link>
+  </div>
 );
